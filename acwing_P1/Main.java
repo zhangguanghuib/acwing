618a8ef0480b9101123f922adab0f660b04bc27a
Guanghui Zhang <zhangguanghuib@126.com>
2019-11-03 11:47:00 +0800

    A+B problem submitted

---
+++ acwing_P1/Main.java
@@ -0,0 +1,13 @@
+package acwing_P1;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+        System.out.println(a + b);
+    }
+}
