9001e22453ca1e92cfdb39b083b1f3e630835dae
Guanghui Zhang <guazha@microsoft.com>
2023-11-27 14:10:11 +0000

    Reverse string

---
+++ Promise/P1-Reverse each string.ts
@@ -0,0 +1,13 @@
+function reverseChunks(input: string): Promise<string>[] {
+    let arrs = input.split('').reverse().join('').split(' ');
+    return arrs.map(chunk => new Promise<string>((resolve) => {
+        resolve(chunk.split('').reverse().join(''));
+    }));
+}
+
+let input : string = 'this is a long string that will be split into chunks and each chunk will be reversed';
+const promises = reverseChunks(input);
+Promise.all(promises).then(results => {
+    let ret: string = results.join(' ');
+    console.log(ret);
+});
